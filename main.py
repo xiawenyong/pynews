b932d5d1c0ab47ef91a39c2a41fd981ef32cda09
wenyong.xia <wenyong.xia@gmail.com>
2025-01-18 23:27:32 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+def main():
+    print("欢迎使用通义灵码！")
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    """返回两个数的和"""
+    return a + b
+
+def subtract(a, b):
+    """返回两个数的差"""
+    return a - b
+
+def multiply(a, b):
+    """返回两个数的积"""
+    return a * b
+
+def divide(a, b):
+    """返回两个数的商"""
+    if b == 0:
+        raise ValueError("除数不能为零")
+    return a / b
